0ca826f3d01f38fd9fb186c34224d8731840646a
Sune Simonsen <sune@we-knowhow.dk>
2012-09-20 21:52:02 +0200

    initial test of single selection model

--- test/spec/SelectionSpec.js
+++ test/spec/SelectionSpec.js
@@ -1,2 +1,54 @@
+function createItems(size) {
+    var result  = [];
+
+    for (var i = 0; i < size; i += 1) {
+        result.push({
+            id: 'item' + (i + 1),
+            selected: ko.observable(false),
+            focused: ko.observable(false)
+        });
+    }
+    return result;
+}
+
 describe('Selection', function () {
+    var model, element;
+    beforeEach(function () {
+        model = {
+            items: ko.observableArray(createItems(10)),
+            selection: ko.observableArray(),
+            focus: ko.observable()
+        };
+    });
+
+    describe('Single selection', function () {
+        beforeEach(function () {
+            element = useTestElement('#single');
+            ko.applyBindings(model, element);
+        });
+
+        describe('with no selection', function () {
+            it('has no elements marks as selected', function () {
+                expect(element).selectionCountToBe(0);
+            });
+
+            it('selects the clicked element', function () {
+                rightClick($('#item3'));
+                expect(element).selectionCountToBe(1);
+            });
+        });
+
+        describe('with one selected item', function () {
+            beforeEach(function () {
+                rightClick($('#item2'));
+            });
+
+            it('moves the selection to the clicked element', function () {
+                rightClick($('#item3'));
+                expect(element).selectionCountToBe(1);
+                expect($('#item3')).toHaveClass('selected');
+                expect($('#item2')).toNotHaveClass('selected');
+            });
+        });
+    });
 });
--- test/spec/SpecHelper.js
+++ test/spec/SpecHelper.js
@@ -1,9 +1,27 @@
+function useTestElement(selector) {
+    var container = $('#test');
+    container.empty();
+    var testElement = $(selector).clone();
+    testElement.appendTo(container);
+    return testElement[0];
+}
+
+function rightClick(element) {
+    var e = jQuery.Event("mousedown", { which: 1});
+    element.trigger(e);
+}
+
 beforeEach(function() {
-  this.addMatchers({
-    toBePlaying: function(expectedSong) {
-      var player = this.actual;
-      return player.currentlyPlayingSong === expectedSong && 
-             player.isPlaying;
-    }
-  });
+
+    this.addMatchers({
+        selectionCountToBe: function (expectedCount) {
+            return $('.selected', this.actual).length === expectedCount;
+        },
+        toHaveClass: function (expectedClass) {
+            return $(this.actual).hasClass(expectedClass);
+        },
+        toNotHaveClass: function (unexpectedClass) {
+            return !$(this.actual).hasClass(unexpectedClass);
+        }
+    });
 });
